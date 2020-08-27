b9c74fd8e177fdb327026789962b6bf6c766c391
alsritter <3320447910@qq.com>
2020-08-27 17:27:49 +0800

    添加基本的依赖以及 pojo

---
+++ src/main/java/com/alsritter/FixSystemApplication.java
@@ -0,0 +1,15 @@
+package com.alsritter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author alsritter
+ * @version 1.0
+ **/
+@SpringBootApplication
+public class FixSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FixSystemApplication.class, args);
+    }
+}
---
+++ src/main/java/com/alsritter/pojo/Student.java
@@ -0,0 +1,67 @@
+package com.alsritter.pojo;
+
+
+public class Student {
+
+  private String studentId;
+  private long uright;
+  private String name;
+  private String gender;
+  private String password;
+  private String phone;
+
+
+  public String getStudentId() {
+    return studentId;
+  }
+
+  public void setStudentId(String studentId) {
+    this.studentId = studentId;
+  }
+
+
+  public long getUright() {
+    return uright;
+  }
+
+  public void setUright(long uright) {
+    this.uright = uright;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+}
